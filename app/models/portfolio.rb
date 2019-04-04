ff43ee62106250ccfef304e86a4f344d7f956363
Andrew Talle <andrew.talle@ibm.com>
2019-04-03 23:20:39 -0700

    add technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
+	has_many :technologies
+	include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.react
-  	where(subtitle: 'React')
+    where(subtitle: 'React')
   end
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
