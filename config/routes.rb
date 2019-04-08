71452dcf50fc02cfd8c03efa8528626951a84ce4
Andrew Talle <andrew.talle@ibm.com>
2019-04-07 22:40:35 -0700

    made auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'react-items', to: 'portfolios#react'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
